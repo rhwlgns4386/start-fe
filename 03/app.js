68ea4c87cb2dc440d16c9854a63d40e4e87f4c80
hunko <rhwlgns4386@naver.com>
2021-03-28 04:41:50 +0900

    4주차 퀴즈 수정

--- 03/app.js
+++ 03/app.js
@@ -1,16 +1,88 @@
-var sum=0
+//console.log(04);
 
-while(1){
+//console.log(window.outerWidth);
+//console.log(window.name);
+//window.open('http://www.google.co.kr');
+// location.href="http://www.google.co.kr";
+// location.reload();
+//history.back();
 
-var input=window.prompt("숫자를 입력해주세요")
+//console.log(navigator.userAgent);
+//console.log(navigator.language);
+//console.log(navigator.platform);
 
-if(input==null){
+// var debug = document.getElementById("debug");
+// console.log(debug);
 
-break
+//var box = document.querySelector(".box");
+// var box2 = document.querySelectorAll(".box");
+// console.log(box2);
 
-}
+// var debug = document.querySelector("#debug");
+// console.log(debug);
 
-sum=sum+Number(input);
+// var div = document.createElement("div");
+// div.style.border = "1px solid red";
+// div.innerHTML = "Hello";
 
-}
-alert(sum)
+// document.body.appendChild(div);
+
+// var span = document.createElement('span');
+// var textNode = document.createTextNode('ksb');
+// span.appendChild(textNode);
+// document.querySelector("#debug").appendChild(span);
+
+// const debug = document.getElementById('debug');
+// debug.remove();
+
+// const list = document.getElementById('list');
+// list.removeChild(list.children[0]);
+
+// document.body.addEventListener('click',function(){
+//     console.log('click');
+// });
+
+
+// function clickBody(){
+//     console.log("click");
+// }
+// document.body.addEventListener('click',clickBody);
+
+// document.querySelector(".box").addEventListener('click',function(){
+//     console.log('click');
+// });
+
+// var count = 0;
+// function popUp(event){
+//     console.log(++count);
+//     console.log(arguments, event, event.type, event.currentTarget);
+// }
+// document.body.addEventListener('click',popUp);
+// document.body.addEventListener('click',function(){
+//     console.log("click");
+// });
+
+// var list = document.querySelector("#list");
+// for(var i =0; i<list.children.length; i++){
+//     list.children[i].addEventListener("click",function(){
+//         console.log(1);
+//     });
+// }
+
+// var app = document.querySelector("#list");
+// app.addEventListener("click",function(event){
+//     if(event.target.nodeName == "LI"){
+//         console.log("li");
+//         event.stopPropagation();
+//     }
+// });
+
+// document.body.addEventListener("click",function(){
+//     console.log("body");
+// });
+
+//var link = document.querySelector("#link");
+//link.addEventListener('click',function(event){
+//    console.log(2);
+//    event.preventDefault();
+//});
