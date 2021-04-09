c5adc575433194dbfb67cc2cf4cafbc794e5c682
hunko <rhwlgns4386@naver.com>
2021-04-10 08:42:45 +0900

    퀴즈  2 3

--- 06/quiz3.js
+++ 06/quiz3.js
@@ -7,57 +7,87 @@ const $resetBtn=document.querySelector(".reset-btn");
 const $toggleBtn=document.querySelector(".toggle-btn");
 const $imageBtn=document.querySelector(".image-btn");
 
-const img='<img src= https://i.imgur.com/69NjYBH.png>';
+const boxString=".box";
 var isOpacity=false;
 
-$addRed.addEventListener("click",event=>{
-    boxs=document.querySelectorAll(".box");
-    for(var i=0; i<boxs.length;i++){
-        boxs[i].className="box red";
-    }
-    console.log($box.className);
-})
 
-$addBtn.addEventListener("click",event=>{
-    child=document.createElement('div');
-    child.className="box";
-    document.body.appendChild(child);
-})
-$deleteBtn.addEventListener("click",event=>{
-    document.querySelector(".box").remove();
-})
+function addRed(){
+    $addRed.addEventListener("click",event=>{
+        boxs=document.querySelectorAll(boxString);
+        for(var i=0; i<boxs.length;i++){
+            boxs[i].className="box red";
+        }
+    })
+    
+}
 
-$textBtn.addEventListener("click",event=>{
-    var boxs=document.querySelectorAll(".box");
-    for(var i=0; i<boxs.length;i++){
-        boxs[i].innerHTML=$text.value;
-    }
-})
+function addClick(){
+    $addBtn.addEventListener("click",event=>{
+        child=document.createElement('div');
+        child.className="box";
+        document.body.appendChild(child);
+    })
+    
+}
 
-$resetBtn.addEventListener("click",event=>{
-    var boxs=document.querySelectorAll(".box");
-    console.log(event.target);
+function deleteClick(){
+    $deleteBtn.addEventListener("click",event=>{
+        document.querySelector(boxString).remove();
+    })
+    
+}
+
+function textChage(){
+    $textBtn.addEventListener("click",event=>{
+        var boxs=document.querySelectorAll(boxString);
+        for(var i=0; i<boxs.length;i++){
+            boxs[i].innerHTML=$text.value;
+        }
+    })
+    
+}
+
+function resetClick(){
+    $resetBtn.addEventListener("click",event=>{
+    var boxs=document.querySelectorAll(boxString);
     for(var i=0; i<boxs.length;i++){
         boxs[i].remove();
     }
 })
+}
+function opacityClick(){
+    $toggleBtn.addEventListener("click",event=>{
+        const $boxs=document.querySelectorAll(boxString);
+        var opacity=null;
+        if($boxs[0].style.opacity==''){
+            $boxs[0].style.opacity=100;
+        }
+        opacity=($boxs[0].style.opacity==0) ? 100:0
+        console.log(opacity);
+        for(var i=0; i<$boxs.length;i++){
+            $boxs[i].style.opacity=opacity;
+        }
+    })
+    
+}
+function imageClick(){
+    const img='<img src= https://i.imgur.com/69NjYBH.png>';
+    $imageBtn.addEventListener("click",event=>{
+        $box=document.querySelector(boxString);
+        if($box==null){
+            child=document.createElement('div');
+            child.className="box";
+            document.body.appendChild(child);  
+            $box=child;
+        }
+        $box.innerHTML=img;
+    })
+}
 
-$toggleBtn.addEventListener("click",event=>{
-    const $boxs=document.querySelectorAll(".box");
-    var opacity=isOpacity ? 100:0
-    isOpacity=!isOpacity;
-    for(var i=0; i<$boxs.length;i++){
-        $boxs[i].style.opacity=opacity;
-    }
-})
-
-$imageBtn.addEventListener("click",event=>{
-    $box=document.querySelector(".box");
-    if($box==null){
-        child=document.createElement('div');
-        child.className="box";
-        document.body.appendChild(child);  
-        $box=child;
-    }
-    $box.innerHTML=img;
-})
+addRed();
+addClick();
+deleteClick();
+textChage();
+resetClick();
+opacityClick();
+imageClick();
